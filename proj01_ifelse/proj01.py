9b09ea572cb2ea819061bd3b6a6ee7d6659602e6
ashlynpierson <40171551+ashlynpierson@users.noreply.github.com>
2018-07-09 13:34:00 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -5,11 +5,32 @@
 
 # Part I:
 # This program asks the user for his/her name and grade.
-#Then, it prints out a sentence that says the number of years until they graduate.
+# Then, it prints out a sentence that says the number of years until they graduate.
 
+
+
+# var name          input from user
+# user_age = raw_input("Enter you age: ")
+# print "you are " + user_age + " years old"
+# print int(user_age) + 10
+# print 5, "cat"
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
 
+#conditional
+age = 18
+if age > 16:
+    print "you can drive!"
+    print "this is still in the if statement"
+    print "over", 16
+elif age == 16:
+    print "you can drive in some states"
+elif age > 95 or age < 16:
+    print "you can't drive!"
+
+
+
 
 # If you complete extensions, describe your extensions here!
+
